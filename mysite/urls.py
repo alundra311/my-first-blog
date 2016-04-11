a6c7e92f65f1256993b2eb75032f693398a507fd
alundra311 <obsines311@yahoo.com>
2016-04-11 15:55:55 +0800

    Fixed warnings in urls.py.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
